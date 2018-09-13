84e68ca215a18398c40d5eb4a40f947fad436f6e
Rafael Takasu <rafatakasu@gmail.com>
2018-09-13 15:18:25 -0700

    added google analytics

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ import Projects from './Projects.js';
 import Topbar from './Topbar.js';
 import { BrowserRouter, Route, Switch, Link, IndexRoute, hashHistory } from 'react-router-dom'
 import { ParallaxProvider } from 'react-scroll-parallax';
+import ReactGA from 'react-ga';
+
 
 class App extends Component {
     constructor(props) {
         super(props);
+
+        ReactGA.initialize('UA-125812817-1');
+        ReactGA.pageview(window.location.pathname + window.location.search);
 
         this.toggle = this.toggle.bind(this);
         this.state = {
